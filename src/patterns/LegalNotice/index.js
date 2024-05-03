44c68a19b2eed93345493549166b283cc0aceaad
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2024-05-03 18:18:20 +0200

    modyfied font-sizes and line-heights

---
+++ src/patterns/LegalNotice/index.js
@@ -0,0 +1,11 @@
+import SectionFrame from "../SectionFrame";
+
+const LegalNotice = () => {
+    return (
+        <SectionFrame section="legal-notice">
+            <div className="legal-notice"></div>
+        </SectionFrame>
+    );
+};
+
+export default LegalNotice;
